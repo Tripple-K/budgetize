1db61537fed6ec0eba5641d45976af2dd0afe67d
Pfriedrix <dnl.krsvch@gmail.com>
2022-02-09 19:03:32 +0200

    Initial Commit

---
+++ budgetize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  budgetize
+//
+//  Created by Danylo Krysevych on 09.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ budgetize/budgetizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetizeApp.swift
+//  budgetize
+//
+//  Created by Danylo Krysevych on 09.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct budgetizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
