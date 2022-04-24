9a403cadda2ae3a76c18a3be50ae161d6c0a2f81
Kazzer <kazzer.nd@gmail.com>
2022-04-24 21:39:41 +0300

    IDK commit
    who the fuck knows what is going on here

--- budgetize/Views/AccountForm.swift
+++ budgetize/Views/AccountForm.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct AccountForm: View {
+    
+//    var account = Account()
+    
+    @State var accountName = ""
+    @State var accountCurrency: CurrencyType = .usd
+    var accountType: AccountType = .debitCard
+//    @State var color
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Let's create your first account!")
+                .font(.largeTitle.bold())
+                .padding()
+            HStack {
+                Text("Name")
+                TextField("Name", text:$accountName)
+            }
+            Text("What's account currency?")
+            Picker("What's your account currency?", selection: $accountCurrency) {
+                ForEach(CurrencyType.allCases, id: \.self)  { currency in
+                    Text(currency.rawValue)
+                        .tag(currency)
+
+                }
+            }.pickerStyle(WheelPickerStyle())
+//            ColorPicker("Choose account color", selection: <#T##Binding<CGColor>#>)
+//
+            
+            Spacer()
+            
+        }
     }
 }
 
@@ -18,3 +48,11 @@ struct AccountForm_Previews: PreviewProvider {
         AccountForm()
     }
 }
+
+
+//@DocumentID var id: String?
+//var userId: String
+//var color: String
+//var accountType: AccountType
+//var currency: CurrencyType
+//var name: String
