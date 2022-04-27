d72500c0418e4162989cbd7ecaa4f507c5ad6df2
Pfriedrix <78740302+pfriedrix@users.noreply.github.com>
2022-04-27 15:10:23 +0300

    correct model default values

--- budgetize/Views/Account.swift
+++ budgetize/Views/Account.swift
@@ -3,21 +3,12 @@ import FirebaseFirestoreSwift
 
 struct Account: Identifiable, Codable {
     @DocumentID var id: String?
-    var userId: String
-    var color: String
-    var type: AccountType
-    var currency: CurrencyType
-    var name: String
-    var balance: Double
-    
-    init () {
-        self.userId = ""
-        self.color = ""
-        self.type = .saving
-        self.currency = .eur
-        self.name = ""
-        self.balance = 0
-    }
+    var userId: String = ""
+    var color: String = ""
+    var type: AccountType = .saving
+    var currency: CurrencyType = .usd
+    var name: String = "Account"
+    var balance: Double = 0
 }
 
 
